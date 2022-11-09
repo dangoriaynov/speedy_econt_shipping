8b49dfc38202bce73d0fec2687fb539cc4d9e5b1
Dan Goriaynov <dgoriaynov@vmware.com>
2022-11-09 02:00:44 +0200

    stupid version change logic

--- speedy_econt_shipping.php
+++ speedy_econt_shipping.php
@@ -6,7 +6,7 @@
  * Author:            Dan Goriaynov
  * Author URI:        https://github.com/dangoriaynov
  * Plugin URI:        https://github.com/dangoriaynov/speedy_econt_shipping
- * Version:           1.5.4
+ * Version:           1.5.5
  * WC tested up to:   6.1
  * License:           GNU General Public License, version 2
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.en.html
